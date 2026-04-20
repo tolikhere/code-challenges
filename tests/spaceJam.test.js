0a41d9ff84a34717a8a30afc998d99e14064ae45
Anatoliy <fenixdurangoadrenalin@gmail.com>
2026-04-20 23:06:07 +0300

    test: implement test suite for Space Jam challenge
        - Created data-driven tests using logGroup and assertEquals.
        - Added test cases covering extra whitespace and casing.

---
+++ tests/spaceJam.test.js
@@ -0,0 +1,18 @@
+import { spaceJam } from "../js/spaceJam.js";
+import { assertEquals } from "../utils/assertEquals.js";
+import { logGroup } from "../utils/logGroup.js";
+
+const testData = [
+  { expected: "F  R  E  E  C  O  D  E  C  A  M  P", arg: "freeCodeCamp" },
+  {
+    expected: "F  R  E  E  C  O  D  E  C  A  M  P",
+    arg: "   free   Code   Camp   ",
+  },
+  { expected: "H  E  L  L  O  W  O  R  L  D  ?  !", arg: "Hello World?!" },
+  { expected: "C  @  T  $  &  D  0  G  $", arg: "C@t$ & D0g$" },
+  { expected: "A  L  L  Y  O  U  R  B  A  S  E", arg: "allyourbase" },
+];
+
+logGroup("S  P  A  C  E  J  A  M", () => {
+  testData.forEach((test) => assertEquals(test.expected, spaceJam, test.arg));
+});
