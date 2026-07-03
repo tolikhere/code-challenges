ed1485cbac733c686885aa5f2e130d9c6ac6ba8f
Anatoliy <fenixdurangoadrenalin@gmail.com>
2026-07-03 23:49:24 +0300

    refactor: Add JSDoc documentation to the function

--- js/freecodecamp/2026/07/hexGenerator.js
+++ js/freecodecamp/2026/07/hexGenerator.js
@@ -1,3 +1,22 @@
+/*
+Given a named CSS color string, generate a random hexadecimal (hex) color code that is dominant in the given color.
+
+    The function should handle "red", "green", or "blue" as an input argument.
+    If the input is not one of those, the function should return "Invalid color".
+    The function should return a random six-character hex color code where the input color value is greater than any of the others.
+    Example of valid outputs for a given input:
+
+Input 	Output
+"red" 	"FF0000"
+"green" 	"00FF00"
+"blue" 	"0000FF"
+*/
+
+/**
+ * Generate a random hexadecimal (hex) color code that is dominant in the given color.
+ * @param {string} color - A named CSS color.
+ * @returns Returns a random hexadecimal color.
+ */
 export function generateHex(color) {
   const colors = {
     red: null,
@@ -21,6 +40,12 @@ export function generateHex(color) {
     .toUpperCase();
 }
 
+/**
+ * Generate a random rgb color from the input. Maximum rgb color is 255 and minimum is 0.
+ * @param {number} maxRange - Maximum range for a random number.
+ * @param {number} minRange - Optional minimum range for a random number. Default is 0.
+ * @returns Random rgb color base on the input.
+ */
 function getRandomRGBColor(maxRange, minRange = 0) {
   return minRange + Math.floor(Math.random() * (maxRange - minRange + 1));
 }
